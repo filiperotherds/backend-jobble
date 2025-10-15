932122a628cb7d86cbe342d81cee1e2a1005039d
Filipe Rother de Souza <filiperother@icloud.com>
2025-10-15 17:26:37 -0300

    add zod validation error

--- src/common/pipes/zod-validation.pipe.ts
+++ src/common/pipes/zod-validation.pipe.ts
@@ -1,5 +1,6 @@
 import { PipeTransform, BadRequestException } from '@nestjs/common'
 import { ZodError, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
@@ -10,9 +11,9 @@ export class ZodValidationPipe implements PipeTransform {
     } catch (error) {
       if (error instanceof ZodError) {
         throw new BadRequestException({
-          errors: error.format(),
           message: 'Validation failed',
           statusCode: 400,
+          errors: fromZodError(error),
         })
       }
 
