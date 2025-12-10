c280cb48661e4a971112b0a95036fe31b234f541
Filipe Rother de Souza <filiperother@icloud.com>
2025-12-10 15:57:40 -0300

    remove cors config

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  })
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  // })
 
   await app.listen(process.env.PORT ?? 3333, '0.0.0.0')
 }
