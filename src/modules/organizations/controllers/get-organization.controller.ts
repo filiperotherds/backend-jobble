8729185bb8c49a0d8c4c0f6d50468ab1e5caae5e
Filipe Rother de Souza <filiperother@icloud.com>
2026-01-09 10:22:40 -0300

    config organizations module

--- src/modules/organizations/controllers/get-organization.controller.ts
+++ src/modules/organizations/controllers/get-organization.controller.ts
@@ -4,12 +4,12 @@ import { JwtAuthGuard } from '@/modules/auth/guards/jwt-auth.guard'
 import { CurrentUser } from '@/common/decorators/current-user-decorator'
 import { TokenPayload } from '@/modules/auth/strategies/jwt.strategy'
 
-@Controller('/users')
+@Controller('/organizations')
 @UseGuards(JwtAuthGuard)
-export class GetUserProfileController {
+export class GetOrganizationController {
   constructor(private organizationsService: OrganizationsService) {}
 
-  @Get('/profile')
+  @Get()
   async getOrganization(@CurrentUser() { ctx }: TokenPayload) {
     if (!ctx.orgId) {
       throw new BadRequestException('Missing params')
@@ -17,8 +17,9 @@ export class GetUserProfileController {
 
     const orgId = ctx.orgId
 
-    const user = await this.organizationsService.getOrganizationById(orgId)
+    const organization =
+      await this.organizationsService.getOrganizationById(orgId)
 
-    return user
+    return organization
   }
 }
---
+++ src/modules/organizations/organizations.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'
+import { PrismaService } from '@/database/prisma/prisma.service'
+import { OrganizationsService } from './organizations.service'
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [OrganizationsService, JwtAuthGuard, PrismaService],
+  exports: [OrganizationsService],
+})
+export class UsersModule {}
